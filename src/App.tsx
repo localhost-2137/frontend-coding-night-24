651977cb069664324b1385801eb12b206f39bff5
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-12-06 22:09:18 +0100

    feat: add map using leaflet and mars trek api

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { useLocation, useRoutes } from "react-router-dom";
+import {useLocation, useRoutes} from "react-router-dom";
 import Header from "./Layout/Header";
-import { ThemeProvider } from "./components/theme-provider";
-import { cloneElement } from "react";
+import {ThemeProvider} from "./components/theme-provider";
+import {cloneElement} from "react";
+import MapWrapper from "@/components/map/mapWrapper.tsx";
 
 export default function App(): JSX.Element | null {
-  const element = useRoutes([
-    {
-      path: "/",
-      element: <Header />,
-      children: [],
-    },
-  ]);
+    const element = useRoutes([
+        {
+            path: "/",
+            element: <Header/>,
+            children: [
+                {
+                    path: "/map",
+                    element: <div>
+                        <MapWrapper/>
+                    </div>,
+                }
+            ],
+        },
+    ]);
 
-  const location = useLocation();
-  if (!element) return null;
+    const location = useLocation();
+    if (!element) return null;
 
-  return (
-    <ThemeProvider>
-      {cloneElement(element, { key: location.pathname })}
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            {cloneElement(element, {key: location.pathname})}
+        </ThemeProvider>
+    );
 }
